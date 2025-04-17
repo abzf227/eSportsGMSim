7f35baf64248952502edfa805a470ab936753a1b
abzf227 <andrewbzfeng@hotmail.com>
2025-04-17 16:02:11 -0500

    Created PlayerLanguage Class

--- Player.cs
+++ Player.cs
@@ -1,10 +1,13 @@
 using System;
+using System.Collections.Generic;
 
 public class Player {
     //general reference stuff
     private string inGameName;
     private string realName;
     private uint age;
+    private string nationality; //isn't important BUT determintes language which is very important
+    private List<PlayerLanguage> languages = new List<PlayerLanguage>; //generally 1 but can be 2 and sometimes 3
 
     //actually important stuff (well age is important too because it has a factor in a lot of things)...similar to nba2k, has a rating out of 100 + a final OVR
     private uint mechanics;  //bread and butter, basically the most important stat
---
+++ PlayerLanguage.cs
@@ -0,0 +1,27 @@
+using System;
+
+public class PlayerLanguage {
+    private string pLang;
+    private double fluency; //determines how fluent from 0.0 - 10.0 the player is...determines communication skills. english tends to be on it
+
+    public PlayerLanguage() {
+        pLang = "English";
+        fluency = 10.0; //max fluency for english speaker...americans....
+    }
+
+    public PlayerLangugae(double fluency) {
+        pLang = "English";
+        fluency = this.fluency; //default to english, secondary language
+    }
+
+    public PlayerLanguage(string pLang) {
+        this.pLang = pLang;
+        fluency = 10.0; //non-native English speaker native language 
+    }
+
+    public PlayerLanguage(string pLang, double fluency) {
+        this.pLang = pLang;
+        this.fluency = fluency; //bilingual king/queen
+    }
+
+}
